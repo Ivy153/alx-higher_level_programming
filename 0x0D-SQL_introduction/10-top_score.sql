cc9654c9bcddc3edf71b7bed1345860f6430482c
Ivy153 <mariayvonne153@gmail.com>
2023-09-19 20:31:07 +0100

    top score

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,5 @@
+-- lists all records of the table second_table from the database hbtn_0c_0
+-- records should be ordered in descending order
+SELECT name, score
+FROM second_table
+ORDER BY score DESC;
